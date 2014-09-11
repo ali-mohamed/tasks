98f3922db78ce58ea44a14eeb0f6dc0b51b01f35
ali-mohamed <thedragon519@gmail.com>
2014-09-12 00:32:40 +0300

    My First Task - First Part

---
+++ task1.rb
@@ -0,0 +1,20 @@
+class Numeric
+  @@currencies = {  egp: 1,  dollar: 7.15,  euro: 9.26,  yen: 0.068, 
+				   egps: 1, dollars: 7.15, euros: 9.26, yens: 0.068 }
+ 
+  def convert_currency(transfer)
+    self * @@currencies[transfer[:to]] / @@currencies[transfer[:from]]
+  end
+  
+  def self.add_currency(currency_value)
+	currency_value.each do |currency, rate|
+		@@currencies[currency] = rate
+		@@currencies["#{currency}s".to_sym] = rate
+	end		
+   end
+  
+end
+
+puts 3.convert_currency from: :egps ,to: :dollars
+Numeric.add_currency(saudi_riyal: 1.9)
+puts 3.convert_currency from: :egps ,to: :saudi_riyals
